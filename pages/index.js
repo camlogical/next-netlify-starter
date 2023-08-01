03caa200dd2165b3f5ee76fd7f9f7ddbba93e3ac
camlogical <57839516+camlogical@users.noreply.github.com>
2023-08-01 10:08:16 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>CODE404 Profile</title>
+        <title>Code404 Profile</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my Profile!" />
         <p className="description">
-          I'm doing <code>pages/index.js</code>
+          I'm doing <code>pages/index.js/code>
         </p>
       </main>
 
