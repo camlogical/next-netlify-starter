1b5144c293c762e24198772d4119b41431673709
camlogical <57839516+camlogical@users.noreply.github.com>
2023-08-01 10:00:45 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,3 @@
 import styles from './Footer.module.css'
 
-export default function Footer() {
-  return (
-    <>
-      <footer className={styles.footer}>
-        <img href="https://secuhex.com/styles/acui/basic/publicLogoUrl.png" alt="Netlify Logo" className={styles.logo} />
-      </footer>
-    </>
-  )
-}
+
